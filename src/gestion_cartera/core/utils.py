b40dd649558031ec01ab5eabdf53d993c70dedf9
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-23 18:24:04 -0500

    fix(utils): cambiar metodo de instancia 'engine' a metodo de clase

--- src/gestion_cartera/core/utils.py
+++ src/gestion_cartera/core/utils.py
@@ -2,16 +2,15 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 import os
 from sqlalchemy import create_engine
-import yaml
 from ensure import ensure_annotations
 import yaml
 from box.exceptions import BoxValueError
 from box import ConfigBox
 from pathlib import Path
-from gestion_cartera.core.constants import *
 
 class DatabaseConnection:
-    def _get_env(self, stream: str) -> tuple[str]:
+    @classmethod
+    def _get_env(cls, stream: str) -> tuple[str]:
         if stream == 'upstream':
             user = os.getenv('DB_UPSTREAM_USER')
             password = os.getenv('DB_UPSTREAM_PASSWORD')
@@ -23,8 +22,9 @@ class DatabaseConnection:
             server = os.getenv('DB_DOWNSTREAM_SERVER')
             database = os.getenv('DB_DOWNSTREAM_DATABASE')
             return None, None, server, database 
-            
-    def _get_conn(self, stream:str, user, password, server, database) -> str:
+    
+    @classmethod
+    def _get_conn(cls, stream:str, user, password, server, database) -> str:
         if stream == 'upstream':
             conn = f'mssql+pyodbc://{user}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server'
             return conn
@@ -33,13 +33,15 @@ class DatabaseConnection:
             conn = f'mssql+pyodbc://{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server'
             return conn
     
-    def _get_engine(self, conn:str):
+    @classmethod
+    def _get_engine(cls, conn:str):
         return create_engine(conn)
     
-    def engine(self, stream:str):
-        user, password, server, database = self._get_env(stream)
-        conn = self._get_conn(stream, user, password, server, database)
-        engine = self._get_engine(conn)
+    @classmethod
+    def engine(cls, stream:str):
+        user, password, server, database = cls._get_env(stream)
+        conn = cls._get_conn(stream, user, password, server, database)
+        engine = cls._get_engine(conn)
         return engine
 
 
@@ -58,5 +60,4 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 
 if __name__ == '__main__':
-    config = read_yaml(PATH_CONFIG)
-    print(config.table)
+    print(DatabaseConnection.engine('downstream'))

6fa16ae8d5e7d672f4a3e13e3b0bdad2ca439682
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-23 17:41:21 -0500

    chore(constants): aniadir ruta a las tablas como configuraciones

--- src/gestion_cartera/core/constants.py
+++ src/gestion_cartera/core/constants.py
@@ -1,1 +1,4 @@
-PATH_PROJECT = "D:/development_stage/bunisess_intelligence_projects/dile-gestion_cartera-data_project/dile-gestion_cartera-data_engineering/"
+from pathlib import Path
+
+PATH_PROJECT = Path("D:/development_stage/bunisess_intelligence_projects/dile-gestion_cartera-data_project/dile-gestion_cartera-data_engineering/")
+PATH_CONFIG_TABLES = Path("conf/tables.yaml")

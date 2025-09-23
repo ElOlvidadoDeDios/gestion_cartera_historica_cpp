c5712b12baf69b58c2783e435c9c908c2c10943f
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-23 18:09:54 -0500

    feat(constants): consulta sql sobre cartera y moras

--- src/gestion_cartera/core/constants.py
+++ src/gestion_cartera/core/constants.py
@@ -1,4 +1,15 @@
 from pathlib import Path
 
+# Directories paths
 PATH_PROJECT = Path("D:/development_stage/bunisess_intelligence_projects/dile-gestion_cartera-data_project/dile-gestion_cartera-data_engineering/")
+PATH_SQL = Path(PATH_PROJECT, "sql/")
+
+# Config paths
 PATH_CONFIG_TABLES = Path("conf/tables.yaml")
+
+# SQL queries paths
+SQL_CARTERA_MORAS = Path(PATH_SQL, 'cartera_moras.sql').read_text(encoding='utf-8')
+
+
+if __name__ == '__main__':
+    print(PATH_SQL)

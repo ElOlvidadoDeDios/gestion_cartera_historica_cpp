66db0538e374e946db88b98aeee3b5cd9de4d5d6
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-24 17:20:11 -0500

    feat(constants): aniadir ruta de consulta T-SQL sobre la dimension asesor

--- src/gestion_cartera/core/constants.py
+++ src/gestion_cartera/core/constants.py
@@ -8,8 +8,9 @@ PATH_SQL = Path(PATH_PROJECT, "sql/")
 PATH_CONFIG_TABLES = Path("conf/tables.yaml")
 
 # SQL queries paths
+SQL_DIM_ASESOR = Path(PATH_SQL, 'dim_asesor.sql').read_text(encoding='utf-8')
 SQL_CARTERA_MORAS = Path(PATH_SQL, 'cartera_moras.sql').read_text(encoding='utf-8')
 
 
 if __name__ == '__main__':
-    print(PATH_SQL)
+    print(SQL_DIM_ASESOR)

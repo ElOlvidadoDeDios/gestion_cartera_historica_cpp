05c49882f804171f7fc080630bc26b23bf088ab4
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-24 17:21:15 -0500

    feat(pipeline): aniadir pipeline sobre transformacion de la dimension asesor

--- src/gestion_cartera/pipelines/__init__.py
+++ src/gestion_cartera/pipelines/__init__.py
@@ -1,7 +1,13 @@
 from gestion_cartera.core.config import ConfigManager
 from gestion_cartera.core import constants
 from gestion_cartera.components.extract import Extractor
+from gestion_cartera.components.transform import TransformerFactory
 from gestion_cartera.components.load import LoaderFactory
+
+def pipeline_dim_asesor():
+    df_loaded = Extractor.run(constants.SQL_DIM_ASESOR)
+    df_transformed = TransformerFactory().get_transformer('dim_asesor').run(df=df_loaded)
+    LoaderFactory.get_loader('strategic').run(df=df_transformed, table=ConfigManager.tables.dim.asesor)
 
 def pipeline_vars_stock():
     df = Extractor.run(constants.SQL_CARTERA_MORAS)
@@ -9,4 +15,4 @@ def pipeline_vars_stock():
 
 
 if __name__ == '__main__':
-    pipeline_vars_stock()
+    pipeline_dim_asesor()

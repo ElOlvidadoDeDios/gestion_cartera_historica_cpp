c7ec292f44567623db7dc2a88b7e5a6da0b69d28
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-24 10:59:44 -0500

    feat(main): llamar pipeline de variables de stock 'cartera' y 'moras'

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from dotenv import load_dotenv
-load_dotenv(".env")
-
-from hydra import main
-from omegaconf import DictConfig, OmegaConf
-
-from gestion_cartera.pipelines import PipelineDimAsesor
+from gestion_cartera.pipelines import pipeline_vars_stock
 
 
-@main(config_path="conf", config_name="config", version_base=None)
-def run_pipelines(cfg: DictConfig) -> None:
-
-    PipelineDimAsesor.run()
-
-
-if __name__ == "__main__":
-
-    run_pipelines()
+pipeline_vars_stock()

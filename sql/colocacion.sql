b8052381127f5586d13a7fce2914cf6db79b9ec0
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-09-25 10:40:56 -0500

    feat(colocacion): sql

---
+++ sql/colocacion.sql
@@ -0,0 +1,67 @@
+--- =============
+--- NOTAS
+--- =============
+
+/*
+- Sobre creditos extornados (improcedentes)
+- Sobre creditos castigados
+*/
+
+--- =============
+--- PREAMBULO
+--- =============
+
+--- -------------
+--- CTEs
+WITH
+--- -------------
+
+CTE AS (
+--CTE-INICIO. Colocacion en la fecha vigente
+
+------
+SELECT
+------
+	T1.CUENTA,
+	CAST(T1.OTORGA AS DATE) AS Fecha,
+	T1.PAGARE,
+	T2.ID_USER,
+	T1.MONTO_PRESTAMO
+------
+FROM
+------
+    PRESTAMO                              T1
+		INNER JOIN PREEC                  NEXO_PRE ON NEXO_PRE.CUENTA    = T1.CUENTA        AND NEXO_PRE.OTORGA      = T1.OTORGA AND NEXO_PRE.PAGARE = T1.PAGARE
+		INNER JOIN SEGURIDAD.dbo.ANAREC   NEXO_ANA ON NEXO_ANA.ID_ANAREC = NEXO_PRE.ID_ANA  AND NEXO_ANA.FLAG_ANAREC = 'A'
+		INNER JOIN SEGURIDAD.dbo.USUARIOS T2       ON T2.ID_USER         = NEXO_ANA.ID_USER
+------
+WHERE
+------
+	T1.TIPO_PROD     != '52'           AND
+	YEAR(T1.OTORGA)  = YEAR(GETDATE()) AND
+	NEXO_PRE.PERIODO =
+		(
+		SELECT MIN(A.PERIODO) FROM PREEC A WHERE A.CUENTA = NEXO_PRE.CUENTA AND A.OTORGA = NEXO_PRE.OTORGA AND A.PAGARE = NEXO_PRE.PAGARE
+		)
+--CTE-FIN. Colocaciones
+
+)
+--- ==============
+--- MAIN
+--- ==============
+
+SELECT
+	CAST(T.Fecha AS DATE)                           AS Fecha,
+	T.ID_USER                                       AS IdSAsesor,
+    CAST(T.Fecha AS VARCHAR(10)) + '-' + T.ID_USER  AS IdUpdate,
+	COUNT(T.PAGARE)                                 AS [ColocacionNumReal],
+	SUM(T.MONTO_PRESTAMO)                           AS [ColocacionMontoReal]
+FROM
+	CTE T
+GROUP BY
+	T.Fecha,
+	T.ID_USER
+ORDER BY
+	T.Fecha,
+	T.ID_USER
+;

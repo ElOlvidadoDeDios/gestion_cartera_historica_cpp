47aac451ac22ceabfcd4cae29d9f5b80beaa552f
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-10-06 18:06:18 -0500

    feat(variational-variables de stock): load

--- src/gestion_cartera/components/load.py
+++ src/gestion_cartera/components/load.py
@@ -1,30 +1,94 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 from gestion_cartera.core.utils import DatabaseConnection
+from sqlalchemy import text
 from enum import Enum
 from typing import Literal
 
 from gestion_cartera.core.config import ConfigManager
+from datetime import date
 
 # Producto
 class Loader(ABC):
+
     @abstractmethod
+    def run(cls, df: pd.DataFrame, table: str, temporality=None):
+        pass
+
+
+# Estrategias
+
+class StrategyLoaderStrategic(Loader, ABC):
+
+    engine = DatabaseConnection.get_engine('downstream')
+    
+    @classmethod
+    def run(cls, df: pd.DataFrame, table: str, temporality=None):
+        pass
+
+class StrategyLoaderOperational(Loader, ABC):
+
+    # TODO Especificar especifica obtencion de 'engine'
+    
+    @classmethod
     def run(cls, df: pd.DataFrame, table: str):
         pass
 
 
-# Productos concretos
+# Productos/Estrategias concretas
 
-class LoaderStrategic(Loader):
-    engine = DatabaseConnection.get_engine('downstream')
-    
+class LoaderStrategicInitial(StrategyLoaderStrategic):
+
     @classmethod
-    def run(cls, df: pd.DataFrame, table: str):
+    def run(cls, df: pd.DataFrame, table: str, temporality=None):
         df.to_sql(table, con=cls.engine, if_exists='replace', index=False)
         cls.engine.dispose()
 
-class LoaderOperational(Loader):
-    pass
+
+class LoaderStrategicVariational(StrategyLoaderStrategic):
+
+    @classmethod
+    def run(cls, df: pd.DataFrame, table: str, temporality: Literal['at_date', 'on_date']):
+
+        if date == 'at_date':
+            temporality = date.today().strftime("%Y%m")
+
+            with cls.engine.begin() as conn:
+                conn.execute(
+                    text("DELETE FROM :Tabla WHERE Periodo = :Periodo"),
+                    {"Tabla": table, "Periodo": temporality}
+                )
+
+        elif date == 'on_date':
+            temporality = date.today().strftime("%Y%m%d")
+
+            with cls.engine.begin() as conn:
+                conn.execute(
+                    text("DELETE FROM :Tabla WHERE Fecha = :Fecha"),
+                    {"Tabla": table, "Fecha": temporality}
+                )
+
+        df.to_sql(table, con=cls.engine, if_exists="append", index=False)
+        cls.engine.dispose()
+
+
+# Contextos
+
+class LoaderStrategic(StrategyLoaderStrategic):
+
+    def __init__(self, strategy: StrategyLoaderStrategic | None = None, temporality = None) -> None:
+        self._strategy = strategy
+
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy):
+        self._strategy = strategy
+
+    def run(self, df: pd.DataFrame, table: str, temporality=None) -> None:
+        return self.strategy.run(df, table, temporality)
 
 
 # Factory
@@ -48,4 +112,6 @@ class LoaderFactory:
 
 
 if __name__ == '__main__':
-    LoaderFactory.get_loader('hola')
+    loader_strategic = LoaderFactory.get_loader('strategic')
+    loader_strategic.strategy = LoaderStrategicInitial
+    loader_strategic.run(df, ConfigManager.table.fct.stock)

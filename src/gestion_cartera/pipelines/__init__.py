2259ad98f05e5bf497450b0b1c1e4b242cfa98c8
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-10-07 10:48:56 -0500

    fix(pipeline): cambiar temporalidad de actualizacion de fecha a periodo

--- src/gestion_cartera/pipelines/__init__.py
+++ src/gestion_cartera/pipelines/__init__.py
@@ -9,20 +9,24 @@ import pandas as pd
 def pipeline_dim_asesor() -> None:
     df_loaded = Extractor.run(constants.SQL_DIM_ASESOR)
     df_transformed = TransformerFactory().get_transformer('dim_asesor').run(df=df_loaded)
-    LoaderFactory.get_loader('strategic').run(df=df_transformed, table=ConfigManager.table.dim.asesor)
+    loader_factory = LoaderFactory.get_loader('strategic')
+    loader_factory.strategy = LoaderStrategicInitial
+    loader_factory.run(df=df_transformed, table=ConfigManager.table.dim.asesor)
 
 
 def pipeline_fct_stock() -> None:
     df = Extractor.run(constants.SQL_FCT_STOCK)
     loader_factory = LoaderFactory.get_loader('strategic')
     loader_factory.strategy = LoaderStrategicVariational
-    loader_factory.run(df=df, table=ConfigManager.table.fct.stock, temporality='at_date')
+    loader_factory.run(df=df, table=ConfigManager.table.fct.stock)
 
 
 def pipeline_fct_flow() -> None:
     df = Extractor.run(constants.SQL_FCT_FLOW)
-    LoaderFactory.get_loader('strategic').run(df=df, table=ConfigManager.table.fct.flow)
+    loader_factory = LoaderFactory.get_loader('strategic')
+    loader_factory.strategy = LoaderStrategicVariational
+    loader_factory.run(df=df, table=ConfigManager.table.fct.flow)
 
 
 if __name__ == '__main__':
-    pipeline_fct_stock()
+    pipeline_dim_asesor()

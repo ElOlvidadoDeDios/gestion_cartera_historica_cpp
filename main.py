3c16d18871073fa6a08d8d646b6610a5904d257b
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-10-07 12:33:34 -0500

    feat(main): pasar argumentos por consola para indicar tipo de pipeline

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from gestion_cartera.pipelines import pipeline_dim_asesor, pipeline_fct_stock, pipeline_fct_flow
+import argparse
+from gestion_cartera.pipelines import pipeline_initial, pipeline_variational
 
 
-def main():
-    pipeline_dim_asesor()
-    pipeline_fct_stock()
-    pipeline_fct_flow()
+PIPELINES = {
+    'initial': pipeline_initial,
+    'variational': pipeline_variational,
+}
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Ejecutar pipelines de gestión de cartera.")
+    parser.add_argument(
+        "pipeline",
+        choices=PIPELINES.keys(),
+        help="Pipeline a ejecutar: 'initial o 'variational'"
+    )
+    args = parser.parse_args()
+
+    PIPELINES[args.pipeline]()
 
 
 if __name__ == '__main__':

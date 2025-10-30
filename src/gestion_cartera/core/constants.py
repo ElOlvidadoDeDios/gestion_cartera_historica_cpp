681b2aaa3d95a3f836b251c6419566d09d8dbb32
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-10-30 12:24:17 -0500

    fix(nueva ruta del proyecto): constants

--- src/gestion_cartera/core/constants.py
+++ src/gestion_cartera/core/constants.py
@@ -7,7 +7,7 @@ from box import Box
 ### #####
 
 
-PATH_PROJECT = Path("D:/development_stage/bunisess_intelligence_projects/dile-gestion_cartera-data_project/dile-gestion_cartera-data_engineering/")
+PATH_PROJECT = Path("D:/development_stage/bunisess_intelligence_projects/projects/dile-gestion_cartera-data_project/dile-gestion_cartera-data_engineering/")
 PATH_SQL = Path(PATH_PROJECT, "sql/")
 PATH_SQL_STRATEGIC = Path(PATH_SQL, "strategic/")
 PATH_SQL_OPERATIONAL = Path(PATH_SQL, "operational/")
@@ -43,4 +43,4 @@ PATH_ENV = Path(PATH_PROJECT, ".env")
 ### ######
 
 if __name__ == '__main__':
-    print(SQL_FCT_STOCK)
+    print(PATH_CONFIG_TABLE)

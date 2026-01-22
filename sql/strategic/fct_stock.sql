72840eb9ba5437eea77747d4233f56bb2c7b1894
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2026-01-22 18:20:07 -0500

    feat(tea): aniadir indicador tea en fct_stock

--- sql/strategic/fct_stock.sql
+++ sql/strategic/fct_stock.sql
@@ -1,22 +1,31 @@
 WITH
 
-CTE_cartera_moras AS (
+CTE_CARTERA_MORAS AS (
 SELECT * FROM gc_cartera_moras
 ),
 
-CTE_duracion AS (
+CTE_DURACION AS (
 SELECT * FROM gc_duracion
+),
+
+CTE_TEA AS (
+SELECT * FROM gc_tea
 )
 
 SELECT
     T.Periodo,
     T.IdSAsesor,
     T.Cartera,
-    T.Cartera_Inicial,
     T.Mora9,
     T.Mora31,
-    T.Mora31_PeriodoAnterior,
     T.Mora150,
-    ISNULL(NEXO_DUR.Varios, 0) AS Varios
-FROM CTE_cartera_moras T
-    LEFT JOIN CTE_duracion NEXO_DUR ON NEXO_DUR.Periodo=T.Periodo AND NEXO_DUR.IdSAsesor=T.IdSAsesor
+    ISNULL(T_DUR.Varios, 0) AS Varios,
+    ISNULL(T_TEA.TEA, 0) AS TEA
+FROM
+    CTE_CARTERA_MORAS T
+    LEFT JOIN CTE_DURACION T_DUR
+        ON  T_DUR.Periodo = T.Periodo
+        AND T_DUR.IdSAsesor = T.IdSAsesor
+    LEFT JOIN CTE_TEA T_TEA
+        ON  T_TEA.Periodo = T.Periodo
+        AND T_TEA.IdSAsesor = T.IdSAsesor

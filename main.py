9753798f40a8ae229826a3a28e2d9926be93376f
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2026-02-06 18:03:37 -0500

    feat(avance de cartera): main

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from dotenv import load_dotenv
 from gestion_cartera.core.constants import PATH_ENV
 load_dotenv(PATH_ENV)
 import argparse
-from gestion_cartera.pipelines import pipeline_initial, pipeline_variational
+from gestion_cartera.pipelines import pipeline_initial, pipeline_variational, pipeline_operational
 
 
 PIPELINES = {
     'initial': pipeline_initial,
     'variational': pipeline_variational,
+    'operational': pipeline_operational,
 }
 
 def main() -> None:
@@ -15,7 +16,7 @@ def main() -> None:
     parser.add_argument(
         "pipeline",
         choices=PIPELINES.keys(),
-        help="Pipeline a ejecutar: 'initial o 'variational'"
+        help="Pipeline a ejecutar: 'initial, 'variational' o 'operational'"
     )
     args = parser.parse_args()
 
